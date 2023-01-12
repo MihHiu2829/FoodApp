4f9ab647a30368afabdc83eeb067f2300e6cb21f
Minh Hếu <minhheu@Minhs-MacBook-Air.local>
2023-01-12 09:30:13 +0700

    TEst git countinute

--- lib/test.dart
+++ lib/test.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
-class test extends StatefulWidget{
+class test extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     throw UnimplementedError();
   }
-
 }
 
-class appState extends State<test>{
+class appState extends State<test> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(); 
   }
-
-
-
 }
